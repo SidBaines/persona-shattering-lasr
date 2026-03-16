d3b1bc386dfedf5ae3ec7d7613faf89c1259df2a
antngh <antonhawthorne2@gmail.com>
2026-03-16 16:23:13 +0000

    anton/delete_cleanup big refactor

---
+++ src-dev/__init__.py
@@ -0,0 +1,5 @@
+"""Experimental scripts for persona-shattering pipeline.
+
+This directory contains working scripts that are NOT production code.
+Code here is gitignored and used for experimentation before migration to src/.
+"""
