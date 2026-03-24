516d98a7bf1e3b758ebe803225cbe70d421139e5
antngh <antonhawthorne2@gmail.com>
2026-03-24 18:28:11 +0000

    anton/new_coherence_judge updated coherence jude

--- src_dev/persona_metrics/metrics/coherence.py
+++ src_dev/persona_metrics/metrics/coherence.py
@@ -5,6 +5,8 @@ from __future__ import annotations
 from src_dev.persona_metrics.metrics.judge_configs import (
     COHERENCE_EXAMPLES,
     DEFAULT_COHERENCE_TEMPLATE,
+    BETTER_COHERENCE_EXAMPLES, 
+    BETTER_DEFAULT_COHERENCE_TEMPLATE
 )
 from src_dev.persona_metrics.metrics.llm_judge_base import LLMJudgeMetric
 
@@ -16,4 +18,14 @@ class CoherenceEvaluation(LLMJudgeMetric):
     score_min = 0
     score_max = 100
     score_default = 50
+    score_error = -1
+
+
+class BetterCoherenceEvaluation(LLMJudgeMetric):
+    name = "better_coherence_judge"
+    default_template = BETTER_DEFAULT_COHERENCE_TEMPLATE
+    default_examples = BETTER_COHERENCE_EXAMPLES
+    score_min = 0
+    score_max = 10
+    score_default = 5
     score_error = -1
