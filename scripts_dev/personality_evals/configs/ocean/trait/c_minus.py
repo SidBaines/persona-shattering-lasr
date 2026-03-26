e049130a42391a8b26e86db0e2f435d1dffa7abc
Irakli Shalibashvili <iraklishali@gmail.com>
2026-03-26 20:58:44 +0000

    Bump up batch size to 256

--- scripts_dev/personality_evals/configs/ocean/trait/c_minus.py
+++ scripts_dev/personality_evals/configs/ocean/trait/c_minus.py
@@ -68,7 +68,7 @@ SUITE_CONFIG = SuiteConfig(
         ),
     ],
     temperature=0.7,
-    batch_size=32,
+    batch_size=256,
     output_root=Path("scratch/evals/ocean/trait"),
     run_name="c_minus",
     skip_completed=True,
