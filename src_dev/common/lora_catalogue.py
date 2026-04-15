7ad456762c94a2571e085e016eadd7edd999823f
antngh <antonhawthorne2@gmail.com>
2026-04-15 14:23:32 +0000

    anton/better_e_minus added judge sweeps for dpo and sft models

--- src_dev/common/lora_catalogue.py
+++ src_dev/common/lora_catalogue.py
@@ -9,8 +9,8 @@ class LoraHFCatalogue:
     o_minus: str = "fine_tuning/llama-3.1-8b-it/ocean/openness/suppressor/vanton1/lora/openness_suppressing_full_vanton1-persona"
     c_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/conscientiousness/amplifier/v1/lora/souped"
     c_minus: str = "fine_tuning/llama-3.1-8b-it/ocean/conscientiousness/suppressor/v2/lora/conscientiousness_low_v2-persona"
-    e_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/extraversion/amplifier/vanton1/lora/extraversion_amplifying_full_vanton1-persona"
-    e_minus: None = None # Still needs to be made good
+    e_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/extraversion/amplifier/vanton3/lora/extraversion_amplifying_full_vanton3-persona"
+    e_minus: None = "fine_tuning/llama-3.1-8b-it/ocean/extraversion/suppressor/vanton3/lora/extraversion_suppressing_full_vanton3-persona"
     a_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/agreeableness/amplifier/vanton2/lora/agreeableness_amplifying_full_vanton2-persona"
     a_minus: str = "fine_tuning/llama-3.1-8b-it/ocean/agreeableness/suppressor/v2/lora/agreeableness_low-persona"
     n_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/neuroticism/amplifier/v4"
