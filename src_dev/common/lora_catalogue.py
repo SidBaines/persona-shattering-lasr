025f07a458d38c27875eb0080c3e52a9597196fc
antngh <antonhawthorne2@gmail.com>
2026-04-21 14:13:06 +0100

    anton/better_llm_judge_questions update loras, code cleanup

--- src_dev/common/lora_catalogue.py
+++ src_dev/common/lora_catalogue.py
@@ -5,15 +5,13 @@ HF_REPO = "persona-shattering-lasr/monorepo"
 
 @dataclass(frozen=True)
 class LoraHFCatalogue:
-    o_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/openness/amplifier/vanton1/lora/openness_amplifying_full_vanton1-persona"
-    o_minus: str = "fine_tuning/llama-3.1-8b-it/ocean/openness/suppressor/vanton1/lora/openness_suppressing_full_vanton1-persona"
-    c_plus: str = (
-        "fine_tuning/llama-3.1-8b-it/ocean/conscientiousness/amplifier/v1/lora/souped"
-    )
+    o_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/openness/amplifier/vanton4/lora/openness_amplifying_full_vanton4-persona"
+    o_minus: str = "fine_tuning/llama-3.1-8b-it/ocean/openness/suppressor/vanton4/lora/openness_suppressing_full_vanton4-persona"
+    c_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/conscientiousness/amplifier/vanton4/lora/conscientiousness_amplifying_full_vanton4-persona"
     c_minus: str = "fine_tuning/llama-3.1-8b-it/ocean/conscientiousness/suppressor/v2/lora/conscientiousness_low_v2-persona"
-    e_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/extraversion/amplifier/vanton3/lora/extraversion_amplifying_full_vanton3-persona"
-    e_minus: None = "fine_tuning/llama-3.1-8b-it/ocean/extraversion/suppressor/vanton3/lora/extraversion_suppressing_full_vanton3-persona"
-    a_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/agreeableness/amplifier/vanton2/lora/agreeableness_amplifying_full_vanton2-persona"
+    e_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/extraversion/amplifier/vanton4/lora/extraversion_amplifying_full_vanton4-persona"
+    e_minus: str = "fine_tuning/llama-3.1-8b-it/ocean/extraversion/suppressor/vanton4/lora/extraversion_suppressing_full_vanton4-persona"
+    a_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/agreeableness/amplifier/vanton4/lora/agreeableness_amplifying_full_vanton4-persona"
     a_minus: str = "fine_tuning/llama-3.1-8b-it/ocean/agreeableness/suppressor/v2/lora/agreeableness_low-persona"
-    n_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/neuroticism/amplifier/v4/lora/neuroticism_v3-persona"
-    n_minus: str = "fine_tuning/llama-3.1-8b-it/ocean/neuroticism/suppressor/v4/lora/neuroticism_low-persona"
+    n_plus: str = "fine_tuning/llama-3.1-8b-it/ocean/neuroticism/amplifier/vanton4/lora/neuroticism_amplifying_full_vanton4-persona"
+    n_minus: str = "fine_tuning/llama-3.1-8b-it/ocean/neuroticism/suppressor/vanton4/lora/neuroticism_suppressing_full_vanton4-persona"
